25966ea101c79d028d76fb1a5c4d67dac1338b4a
beltcroods <beltcroods@gmail.com>
2021-02-23 15:28:55 -0500

    Initial Commit

---
+++ Foreign-languages-Heads-Up/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foreign-languages-Heads-Up
+//
+//  Created by user180891 on 2/23/21.
+//  Copyright © 2021 user180891. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
