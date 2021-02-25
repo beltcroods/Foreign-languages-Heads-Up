dfe5863898fd469b810931bd08e58841afb744db
beltcroods <beltcroods@gmail.com>
2021-02-25 15:09:44 -0600

    Swift Files for Storyboard added storyboard 6 added

---
+++ Foreign-languages-Heads-Up/ViewController6.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController6.swift
+//  Foreign-languages-Heads-Up
+//
+//  Created by John Horlamus on 2/25/21.
+//  Copyright © 2021 user180891. All rights reserved.
+//
+
+import UIKit
+
+class ViewController6: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
