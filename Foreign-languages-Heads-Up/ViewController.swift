083a4b784e4b45af6767bb4f299cdc8543b235c2
beltcroods <beltcroods@gmail.com>
2021-03-18 14:59:09 -0500

    Randomizer Full working prototype

--- Foreign-languages-Heads-Up/ViewController.swift
+++ Foreign-languages-Heads-Up/ViewController.swift
@@ -24,11 +24,24 @@ class ViewController: UIViewController {
         tap.numberOfTapsRequired = 2
         view.addGestureRecognizer(tap)
         
-        let cardset1 = ["soundtrack": "une bande originale"]
+        let cardset1 = ["soundtrack": "une bande originale"
+                        "network" : "une chaine"
+                        "music video" : "un clip video ; un videoclip"
+                        "entertainment" : "un divertissment"
+                        "documentary" : "un documentaire"
+                        "screen" : "pecran"
+                        "special effects" : "les effets speciaux (m)"
+                        "interview" : "un entretien/une unterview"
+                        "soap opera ; series" : "un feuilleton"
+                        "premiere" : "une premoere"
+                        "subtitles" : "les sous-titres (m)"
+                        "to entertain" : "divertir"
+                        "to broadcast" : "retransmettre"
+                        "to release a movie" : "sortir un film"]
     }
     
     @objc func doubleTapped(){
-        let alert = UIAlertController(title: "Cardset #1", message: "\(cardset1[ "soundtrack"])", preferredStyle: UIAlertControllerStyle.Alert)
+        let alert = UIAlertController(title: "Test", message: "Test", preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
     }
@@ -39,10 +52,49 @@ class ViewController: UIViewController {
     
     @IBAction func Cardset1Button(_ sender: Any) {
         
-        let CurrentCardNumber = Int.random(in: 1...1)
+        let CurrentCardNumber = Int.random(in: 1...13)
         if CurrentCardNumber == 1 {
             TESTUILabel.text = cardset1["soundtrack"]
         }
+        if CurrentCardNumber == 2 {
+            TESTUILabel.text = cardset1["network"]
+        }
+        if CurrentCardNumber == 3 {
+            TESTUILabel.text = cardset1["music video"]
+        }
+        if CurrentCardNumber == 4 {
+            TESTUILabel.text = cardset1["documentary"]
+        }
+        if CurrentCardNumber == 5 {
+            TESTUILabel.text = cardset1["screen"]
+        }
+        if CurrentCardNumber == 6 {
+            TESTUILabel.text = cardset1["special effects"]
+        }
+        if CurrentCardNumber == 7 {
+            TESTUILabel.text = cardset1["interview"]
+        }
+        if CurrentCardNumber == 8 {
+            TESTUILabel.text = cardset1["soap opera"]
+        }
+        if CurrentCardNumber == 9 {
+            TESTUILabel.text = cardset1["premiere"]
+        }
+        if CurrentCardNumber == 10 {
+            TESTUILabel.text = cardset1["subtitles"]
+        }
+        if CurrentCardNumber == 11 {
+            TESTUILabel.text = cardset1["to entertain"]
+        }
+        if CurrentCardNumber == 12 {
+            TESTUILabel.text = cardset1["to broadcast"]
+        }
+        if CurrentCardNumber == 13 {
+            TESTUILabel.text = cardset1["to release a movie"]
+        }
+        
+        
+        
     }
     
 }
