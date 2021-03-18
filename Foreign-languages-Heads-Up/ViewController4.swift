fc0df47053663e00d34f743efff86acb2fd7e1c8
KristiyanIvanov22 <kivanov2180@stu.d214.org>
2021-03-18 16:03:53 -0400

    Worked on Storyboard 4

--- Foreign-languages-Heads-Up/ViewController4.swift
+++ Foreign-languages-Heads-Up/ViewController4.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController4: UIViewController {
+class ViewController4: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let otherLanguageCorrectArray = [""]
+        let englishCorrectArray = [""]
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return otherLanguageCorrectArray.count
+    }
 
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellOtherCorrect", for: indexPath)
+
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+
+        return cell
+        }
+            
+            func tableView2(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                    return englishCorrectArray.count
+            }
+
+                func tableView2(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cellEnglishCorrect", for: indexPath)
+
+                cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+
+                return cell
+                    
+            }
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var OtherLanguageTableView: UITableView!
+    @IBOutlet weak var EnglishTableView: UITableView!
     
 
     /*
